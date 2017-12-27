5a81a3d9760ea2e6ae467876892cd8da50f7e597
Tom Flynn <tom.flynn@samsung.com>
2017-12-27 13:30:44 -0800

    Detect transparency in ASTC.
    Detect transparency in ASTC.  ASTC files will scan their Color Endpoint
    Modes to tell if the file has transparency or not.

--- GVRf/Framework/framework/src/main/jni/objects/textures/bitmap_image.cpp
+++ GVRf/Framework/framework/src/main/jni/objects/textures/bitmap_image.cpp
@@ -15,6 +15,7 @@
 
 #include "bitmap_image.h"
 #include "bitmap_transparency.h"
+#include "astc_transparency.h"
 
 namespace gvr {
 BitmapImage::BitmapImage(int format) :
@@ -129,6 +130,13 @@ void BitmapImage::clearData(JNIEnv* env)
 
 bool BitmapImage::hasAlpha(int format) {
     switch(format) {
+        case GL_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2:
+        case GL_COMPRESSED_RG11_EAC:
+        case GL_COMPRESSED_SIGNED_RG11_EAC:
+        case GL_COMPRESSED_RGBA8_ETC2_EAC:
+        case GL_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC:
+            return true;
+            break;
         case GL_COMPRESSED_RGBA_ASTC_4x4_KHR:
         case GL_COMPRESSED_RGBA_ASTC_5x4_KHR:
         case GL_COMPRESSED_RGBA_ASTC_5x5_KHR:
@@ -157,12 +165,8 @@ bool BitmapImage::hasAlpha(int format) {
         case GL_COMPRESSED_SRGB8_ALPHA8_ASTC_10x10_KHR:
         case GL_COMPRESSED_SRGB8_ALPHA8_ASTC_12x10_KHR:
         case GL_COMPRESSED_SRGB8_ALPHA8_ASTC_12x12_KHR:
-        case GL_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2:
-        case GL_COMPRESSED_RG11_EAC:
-        case GL_COMPRESSED_SIGNED_RG11_EAC:
-        case GL_COMPRESSED_RGBA8_ETC2_EAC:
-        case GL_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC:
-            return true;
+            return astc_has_transparency(mData, mImageSize);
+            break;
         default:
             return false;
     }
